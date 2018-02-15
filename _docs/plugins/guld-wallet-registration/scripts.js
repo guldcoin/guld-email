4f058b4732b9a79ed85d34275e90895cb59be038
Chriss Mejía <me@chrissmejia.com>
2018-02-15 17:18:45 -0600

    Plugins update

---
+++ _docs/plugins/guld-wallet-registration/scripts.js
@@ -0,0 +1,159 @@
+(function () {
+    function generateDoc() {
+        var date = new Date(Date.now());
+        var registrator = document.getElementById('guld_registration_registrator').value;
+        var type = document.getElementById('guld_registration_type').value;
+        var name = document.getElementById('guld_registration_name').value;
+        var number = document.getElementById('guld_registration_number').value;
+
+        if (type.value == 1) {
+            var doc = "; requires only sufficient guld and an open name\n"
+            doc += date.getFullYear() + "/" + date.getDate() + "/" + date.getDay() + " * individual registration\n"
+            doc += "    " + registrator + ":Assets   -1 guld\n"
+            doc += "    " + registrator + ":Expenses:guld:register   1 guld\n"
+            doc += "    guld:Liabilities   1 guld\n"
+            doc += "    guld:Income:register:individual:" + registrator + "   -1 guld\n"
+        } else if (type.value == 2) {
+            var doc = "; m = number of members\n"
+            doc += date.getFullYear() + "/" + date.getDate() + "/" + date.getDay() + " * group registration\n"
+            doc += "    " + registrator + ":Assets   -" + number + " guld\n"
+            doc += "    " + registrator + ":Expenses:guld:register   " + number + " guld\n"
+            doc += "    guld:Liabilities   " + number + " guld\n"
+            doc += "    guld:Income:register:group:groupname   -" + number + " guld\n"
+        } else {
+            var doc = ""
+            doc += date.getFullYear() + "/" + date.getDate() + "/" + date.getDay() + " * device registration\n"
+            doc += "    " + registrator + ":Assets   -0.1 guld\n"
+            doc += "    " + registrator + ":Expenses:guld:register   0.1 guld\n"
+            doc += "    guld:Liabilities   0.1 guld\n"
+            doc += "    guld:Income:register:device:" + registrator + ":devicename   -0.1 guld\n"
+        }
+
+
+        return doc;
+    }
+
+    function copyHtml() {
+        var html =
+            "<div id='guld_registration_modal' class='modal fade' tabindex='-1' role='dialog' aria-labelledby='registration_label' style='display: none;'" +
+            "aria-hidden='true'>" +
+            "  <div class='modal-dialog modal-md' role='document'>" +
+            "    <div class='modal-content'>" +
+            "      <div class='modal-header'>" +
+            "        <h5 class='modal-title' id='registration_label'>Send guld</h5>" +
+            "          <button type='button' class='close' data-dismiss='modal' aria-label='Close'>" +
+            "            <span aria-hidden='true'>×</span>" +
+            "          </button>" +
+            "      </div>" +
+            "      <div class='modal-body'>" +
+            "        <section id='guld-registration-section-form'>" +
+            "          <div class='container'> " +
+            "            <div class='row'> " +
+            "              <div class='col-md-12'> " +
+            "                <form id='guld_registration_form' class='needs-validation' novalidate> " +
+            "                <div class='form-group row'> " +
+            "                  <label for='receiver' class='col-sm-5 col-form-label'>Registration Type</label> " +
+            "                  <div class='col-sm-7'> " +
+            "                    <select id='guld_registration_type' class='form-control'> " +
+            "                      <option value='1' selected>User</option> " +
+            "                      <option value='2'>Group</option> " +
+            "                      <option value='3'>Device</option> " +
+            "                    </select> " +
+            "                  </div> " +
+            "                </div> " +
+            "                <div class='form-group row'> " +
+            "                  <label for='sender' class='col-sm-5 col-form-label'>Guld Username</label> " +
+            "                  <div class='col-sm-7'> " +
+            "                    <input type='text' class='form-control' id='guld_registration_registrator' placeholder='Guld Username' required> " +
+            "                  </div> " +
+            "                </div> " +
+            "                <div id='guld_registration_name_group' class='form-group row' style='display:none'> " +
+            "                  <label for='sender' class='col-sm-5 col-form-label'>Name</label> " +
+            "                  <div class='col-sm-7'> " +
+            "                    <input type='text' class='form-control' id='guld_registration_name' placeholder='Name'> " +
+            "                  </div> " +
+            "                </div> " +
+            "                <div id='guld_registration_number_group' class='form-group row' style='display:none'> " +
+            "                  <label for='amount' class='col-sm-5 col-form-label'>Amount</label> " +
+            "                  <div class='col-sm-7'> " +
+            "                    <input type='number' step='1' class='form-control' id='guld_registration_number' placeholder='1'> " +
+            "                  </div> " +
+            "                </div> " +
+            "                <div class='modal-footer row text-right mb-0 pb-0 pr-0'> " +
+            "                  <div class='col-sm-12'> " +
+            "                    <button class='btn btn-primary' type='submit'>generate</button> " +
+            "                  </div> " +
+            "                </div> " +
+            "                </form> " +
+            "              </div> " +
+            "            </div> " +
+            "          </div> " +
+            "        </section>" +
+            "        <section id='guld-registration-section-done'>" +
+            "          <div class='container'> " +
+            "            <div class='row'> " +
+            "              <div class='col-md-12 text-center'> " +
+            "                Please check your email client, sign and send your transaction" +
+            "              </div> " +
+            "            </div> " +
+            "            <div class='row'> " +
+            "              <div class='col-md-12 text-center mt-3 mb-1'> " +
+            "                <a class='btn btn-primary' id='guld_transaction_another'>Generate another</a>" +
+            "              </div> " +
+            "            </div> " +
+            "          </div> " +
+            "        </section>" +
+            "      </div> " +
+            "    </div> " +
+            "  </div> " +
+            "</div>";
+        return html;
+    }
+
+    // Start
+    var guld_registration = document.createElement("div");
+    guld_registration.id = "guld_registration";
+    guld_registration.innerHTML = copyHtml();
+    document.body.appendChild(guld_registration);
+
+    var form = document.getElementById('guld_registration_form');
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        if (form.checkValidity() === true) {
+            var email = 'public@email.guld';
+            var subject = "Autogenerated transaction";
+            var emailBody = generateDoc();
+            $("#guld-registration-section-form").hide();
+            $("#guld-registration-section-done").show();
+            document.location = "mailto:" + email + "?subject=" + subject + "&body=" + emailBody;
+        }
+        form.classList.add('was-validated');
+    }, false);
+
+    var another = document.getElementById('guld_transaction_another');
+    another.addEventListener('click', function (event) {
+        $("#guld-registration-section-form").show();
+        $("#guld-registration-section-done").hide();
+    }, false);
+
+    var type = document.getElementById('guld_registration_type');
+    type.addEventListener('change', function (event) {
+        $("#guld_registration_name_group").hide();
+        $("#guld_registration_name").prop("required", false);
+        $("#guld_registration_number_group").hide();
+        $("#guld_registration_number").prop("required", false);
+        if (type.value > 1) {
+            $("#guld_registration_name_group").show();
+            $("#guld_registration_name").prop("required", true);
+        }
+        if (type.value == 2) {
+            $("#guld_registration_number_group").show();
+            $("#guld_registration_number").prop("required", true);
+        }
+    }, false);
+
+    $("#guld-registration-section-form").show();
+    $("#guld-registration-section-done").hide();
+
+})();
--- docs/plugins/guld-wallet-grant/scripts.js
+++ docs/plugins/guld-wallet-grant/scripts.js
@@ -1,14 +1,13 @@
 (function () {
     function generateDoc() {
         var date = new Date(Date.now());
-        var sender = document.getElementById('guld_transfer_sender').value;
-        var receiver = document.getElementById('guld_transfer_receiver').value;
-        var amount = document.getElementById('guld_transfer_amount').value;
+        var receiver = document.getElementById('guld_grant_receiver').value;
+        var amount = document.getElementById('guld_grant_amount').value;
 
-        var doc = "; requires signature of the debtor\n"
-        doc += date.getFullYear() + "/" + date.getDate() + "/" + date.getDay() + " * transfer\n"
-        doc += "    " + sender + ":Assets   -" + amount + " guld\n"
-        doc += "    " + sender + ":Expenses   " + amount + " guld\n"
+        var doc = "; requires majority of guld equity to approve\n"
+        doc += date.getFullYear() + "/" + date.getDate() + "/" + date.getDay() + " * Grant for work done\n"
+        doc += "    guld:Liabilities   -" + amount + " guld\n"
+        doc += "    guld:Equity:" + receiver + "   " + amount + " guld\n"
         doc += "    " + receiver + ":Assets   " + amount + " guld\n"
         doc += "    " + receiver + ":Income   -" + amount + " guld\n"
 
@@ -17,38 +16,32 @@
 
     function copyHtml() {
         var html =
-            "<div id='guld_transfer_modal' class='modal fade' tabindex='-1' role='dialog' aria-labelledby='transfer_label' style='display: none;'" +
+            "<div id='guld_grant_modal' class='modal fade' tabindex='-1' role='dialog' aria-labelledby='grant_label' style='display: none;'" +
             "aria-hidden='true'>" +
             "  <div class='modal-dialog modal-md' role='document'>" +
             "    <div class='modal-content'>" +
             "      <div class='modal-header'>" +
-            "        <h5 class='modal-title' id='transfer_label'>Send guld</h5>" +
+            "        <h5 class='modal-title' id='grant_label'>Grant guld</h5>" +
             "          <button type='button' class='close' data-dismiss='modal' aria-label='Close'>" +
             "            <span aria-hidden='true'>×</span>" +
             "          </button>" +
             "      </div>" +
             "      <div class='modal-body'>" +
-            "        <section id='guld-transfer-section-form'>" +
+            "        <section id='guld-grant-section-form'>" +
             "          <div class='container'> " +
             "            <div class='row'> " +
             "              <div class='col-md-12'> " +
-            "                <form id='guld_transfer_form' class='needs-validation' novalidate> " +
-            "                <div class='form-group row'> " +
-            "                  <label for='sender' class='col-sm-5 col-form-label'>Sender Guld Username</label> " +
-            "                  <div class='col-sm-7'> " +
-            "                    <input type='text' class='form-control' id='guld_transfer_sender' placeholder='Sender' required> " +
-            "                  </div> " +
-            "                </div> " +
+            "                <form id='guld_grant_form' class='needs-validation' novalidate> " +
             "                <div class='form-group row'> " +
             "                  <label for='receiver' class='col-sm-5 col-form-label'>Receiver Guld Username</label> " +
             "                  <div class='col-sm-7'> " +
-            "                    <input type='text' class='form-control' id='guld_transfer_receiver' placeholder='Receiver' required> " +
+            "                    <input type='text' class='form-control' id='guld_grant_receiver' placeholder='Receiver' required> " +
             "                  </div> " +
             "                </div> " +
             "                <div class='form-group row'> " +
             "                  <label for='amount' class='col-sm-5 col-form-label'>Amount</label> " +
             "                  <div class='col-sm-7'> " +
-            "                    <input type='number' step='.00000001' class='form-control' id='guld_transfer_amount' placeholder='3.5' required> " +
+            "                    <input type='number' step='.00000001' class='form-control' id='guld_grant_amount' placeholder='3.5' required> " +
             "                  </div> " +
             "                </div> " +
             "                <div class='modal-footer row text-right mb-0 pb-0 pr-0'> " +
@@ -61,16 +54,16 @@
             "            </div> " +
             "          </div> " +
             "        </section>" +
-            "        <section id='guld-transfer-section-done'>" +
+            "        <section id='guld-grant-section-done'>" +
             "          <div class='container'> " +
             "            <div class='row'> " +
             "              <div class='col-md-12 text-center'> " +
-            "                Please check your email client, sign and send your transaction" +
+            "                Please check your email client, sign and send your grant" +
             "              </div> " +
             "            </div> " +
             "            <div class='row'> " +
             "              <div class='col-md-12 text-center mt-3 mb-1'> " +
-            "                <a class='btn btn-primary' id='guld_transaction_another'>Generate another</a>" +
+            "                <a class='btn btn-primary' id='guld_grant_another'>Generate another</a>" +
             "              </div> " +
             "            </div> " +
             "          </div> " +
@@ -83,12 +76,12 @@
     }
 
     // Start
-    var guld_transfer = document.createElement("div");
-    guld_transfer.id = "guld_transfer";
-    guld_transfer.innerHTML = copyHtml();
-    document.body.appendChild(guld_transfer);
+    var guld_grant = document.createElement("div");
+    guld_grant.id = "guld_grant";
+    guld_grant.innerHTML = copyHtml();
+    document.body.appendChild(guld_grant);
 
-    var form = document.getElementById('guld_transfer_form');
+    var form = document.getElementById('guld_grant_form');
     form.addEventListener('submit', function (event) {
         event.preventDefault();
         event.stopPropagation();
@@ -96,19 +89,19 @@
             var email = 'public@email.guld';
             var subject = "Autogenerated transaction";
             var emailBody = generateDoc();
-            $("#guld-transfer-section-form").hide();
-            $("#guld-transfer-section-done").show();
+            $("#guld-grant-section-form").hide();
+            $("#guld-grant-section-done").show();
             document.location = "mailto:" + email + "?subject=" + subject + "&body=" + emailBody;
         }
         form.classList.add('was-validated');
     }, false);
 
-    var another = document.getElementById('guld_transaction_another');
+    var another = document.getElementById('guld_grant_another');
     another.addEventListener('click', function (event) {
-        $("#guld-transfer-section-form").show();
-        $("#guld-transfer-section-done").hide();
+        $("#guld-grant-section-form").show();
+        $("#guld-grant-section-done").hide();
     }, false);
-    $("#guld-transfer-section-form").show();
-    $("#guld-transfer-section-done").hide();
+    $("#guld-grant-section-form").show();
+    $("#guld-grant-section-done").hide();
 
 })();
